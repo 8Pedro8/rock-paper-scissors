10709a250dbd40e9999e1a1697813ca11833005a
Pedram Hosseiniakram <pedrammm@msn.com>
2022-05-12 20:55:03 +0430

    selectionsToStateMapping(): find user state from selections

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+let computerSelection = "";
+let playerSelection = "";
+
 function computerPlay() 
 {
     let randomDecision = Math.floor(Math.random()*3) + 1;
@@ -15,4 +18,42 @@ function computerPlay()
     }
 }
 
-console.log(computerPlay())
+function selectionsToStateMapping(decision = "") {
+    switch(decision) 
+    {
+        case "rockrock":
+        case "paperpaper":
+        case "scissorsscissors":
+            return "Tie";
+
+        case "rockpaper":
+        case "paperscissors":
+        case "scissorsrock":
+            return "loser";
+
+        case "rockscissors":
+        case "paperrock":
+        case "scissorspaper":
+            return "winner";
+
+        default:
+            return "Wrong pair!";
+    }
+}
+
+function playRound(playerSelection = "", computerSelection = "") 
+{
+    let selectionsCombined = playerSelection.toLocaleLowerCase() + computerSelection.toLocaleLowerCase();
+    console.log("pair: " + selectionsCombined);
+
+    let userState = selectionsToStateMapping(selectionsCombined)
+    return userState;
+}
+
+playerSelection = "rock";
+computerSelection = computerPlay();
+
+console.log("computer: " + computerSelection);
+console.log("player: " + playerSelection);
+
+console.log("user state is: " + playRound(playerSelection, computerSelection));
